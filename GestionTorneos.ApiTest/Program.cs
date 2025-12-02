5f2164c2b616c6c7e88625abb489248ee92cec9f
alejo10005 <alejonavarrete05@icloud.com>
2025-12-02 16:02:26 -0500

    insercion de datos para nuevo torneo

--- GestionTorneos.ApiTest/Program.cs
+++ GestionTorneos.ApiTest/Program.cs
@@ -1,7 +1,6 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Net.Http.Headers;
-using System.Text;
+﻿
+using GestionTorneosDeportivos.Modelos;
+using Newtonsoft.Json;
 
 namespace GestionTorneos.ApiTest
 {
@@ -11,16 +10,41 @@ namespace GestionTorneos.ApiTest
         {
             var httpClient = new HttpClient();
             var rutaTorneos = "api/Torneos";
-            httpClient.BaseAddress = new Uri("https://localhost:7215/");
+         
+            httpClient.BaseAddress = new Uri("https://localhost:7155/");
 
             //LECTURA DE DATOS
             var response = httpClient.GetAsync(rutaTorneos).Result;
+        
             var json = response.Content.ReadAsStringAsync().Result;
+           
+
+
+            //INSERCION DE DATOS
+            var nuevoTorneo = new GestionTorneosDeportivos.Modelos.Torneo()
+            {
+                Id = 0,
+                Nombre = "Copa Primavera 2024",
+                Tipo="Mixto",
+                FechaInicio= new DateTime(),
+                FechaFin=new DateTime(),
+                Estado="Jugando"
+            };
+         
+
+            //Invocar al servicio web para insertar nuevo torneo
+            var torneoJson = Newtonsoft.Json.JsonConvert.SerializeObject(nuevoTorneo);
             
+            var content = new StringContent(torneoJson, System.Text.Encoding.UTF8, "application/json");
+            
+            response = httpClient.PostAsync(rutaTorneos, content).Result;
+          
+            json = response.Content.ReadAsStringAsync().Result;
             
 
+            Console.WriteLine(json);
+            
+            Console.ReadLine();
         }
-    
     }
-
 }
