f128022afba78cd2b7fb9f9f277052a75e0bab31
alejo10005 <alejonavarrete05@icloud.com>
2025-12-02 16:21:44 -0500

    program de apitest y dockerfile

--- GestionTorneos.ApiTest/Program.cs
+++ GestionTorneos.ApiTest/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using GestionTorneosDeportivos.Modelos;
+﻿using GestionTorneosDeportivos.Modelos;
 using Newtonsoft.Json;
 
 namespace GestionTorneos.ApiTest
@@ -15,11 +14,8 @@ namespace GestionTorneos.ApiTest
 
             //LECTURA DE DATOS
             var response = httpClient.GetAsync(rutaTorneos).Result;
-        
             var json = response.Content.ReadAsStringAsync().Result;
            
-
-
             //INSERCION DE DATOS
             var nuevoTorneo = new GestionTorneosDeportivos.Modelos.Torneo()
             {
@@ -28,22 +24,18 @@ namespace GestionTorneos.ApiTest
                 Tipo="Mixto",
                 FechaInicio= new DateTime(),
                 FechaFin=new DateTime(),
-                Estado="Jugando"
+                Estado="Jugando",
+                
             };
          
-
             //Invocar al servicio web para insertar nuevo torneo
             var torneoJson = Newtonsoft.Json.JsonConvert.SerializeObject(nuevoTorneo);
-            
-            var content = new StringContent(torneoJson, System.Text.Encoding.UTF8, "application/json");
-            
+            var content = new StringContent(torneoJson,  System.Text.Encoding.UTF8, "application/json");
             response = httpClient.PostAsync(rutaTorneos, content).Result;
-          
             json = response.Content.ReadAsStringAsync().Result;
             
 
-            Console.WriteLine(json);
-            
+            Console.WriteLine(json);           
             Console.ReadLine();
         }
     }
